4e9a251fa9ef9d0062327d6e92929610f3db95ec
simonearata <simone.arata1@gmail.com>
2021-04-22 20:15:13 +0200

    first

---
+++ assets/js/script.js
@@ -0,0 +1,38 @@
+$(document).ready(function(){
+  
+  // array delle cose da fare
+  var arrLyst = [
+    'fare spesa', 
+    'andare in palestra', 
+    'fare gli esercizi'
+  ];
+
+  // ciclo per contare l'array e ogni elemento viene inserito in ul
+  for(var i = 0; i < arrLyst.length; i++){
+    var elenco = arrLyst[i];
+
+    var textB = '<li><p>' + elenco + '</p><i class="far fa-trash-alt"></i></li>';
+    $('.list-items ul').append(textB)
+  }
+
+  // cliccando sul cestino eliminiamo l'elemento dalla lista
+  $(document).on('click', '.fa-trash-alt', function(){
+    $(this).parent().remove();
+  })
+
+  //aggiungiamo alla lista il testo inserito dall'utente
+  $('input').keyup(function(event){
+    if(event.which === 13){
+      $(this).val().trim();
+      
+
+    }
+  })
+
+
+
+
+
+
+
+})
